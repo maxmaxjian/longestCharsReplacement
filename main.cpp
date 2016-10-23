42c07c654a813f89a1173f35c0724fcb9ee6372b
Jian Wei <jianwei@Jians-MacBook-Pro.local>
2016-10-23 02:04:08 -0400

    second edit

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <utility>
+// #include <utility>
+#include <iterator>
+#include <algorithm>
 
 class solution {
 public:
   int characterReplacement(const std::string & s, int k) {
-    std::unordered_map<char,std::vector<std::pair<size_t,size_t>>> dict;
+    std::unordered_map<char,std::vector<size_t>> dict;
     for (auto ch : s) {
       if (dict.find(ch) == dict.end()) {
 	// std::cout << ch << std::endl;
@@ -18,11 +20,15 @@ public:
 	  end = s.find_first_not_of(ch, begin);
 	  // std::cout << "end = " << end << std::endl;
 	  if (dict.find(ch) == dict.end())
-	    dict[ch] = std::vector<std::pair<size_t,size_t>>();
-	  if (end == std::string::npos)
-	    dict[ch].push_back(std::make_pair(begin, s.size()));
-	  else
-	    dict[ch].push_back(std::make_pair(begin, end));
+	    dict[ch] = std::vector<size_t>();
+	  if (end == std::string::npos) {
+	    for (size_t i = begin; i < s.size(); ++i)
+	      dict[ch].push_back(i);
+	  }
+	  else {
+	    for (size_t i = begin; i < end; ++i)
+	      dict[ch].push_back(i);
+	  }
 	  begin = s.find_first_of(ch, end);
 	}
 	// if (begin != std::string::npos && end == std::string::npos) {
@@ -34,19 +40,28 @@ public:
     }
 
     for (auto it = dict.begin(); it != dict.end(); ++it) {
-      std::cout << it->first << ":\n";
-      for (auto pr : it->second)
-	std::cout << "[" << pr.first << "," << pr.second << ") ";
+      std::cout << it->first << ": ";
+      // for (auto i : it->second)
+      // 	std::cout << i << " ";
+      std::copy(it->second.begin(), it->second.end(), std::ostream_iterator<size_t>(std::cout, " "));
       std::cout << std::endl;
     }
 
     return 0;
   }
+
+private:
+  int mContiguousAddK(const std::vector<size_t> & indx, size_t mxIdx, int k) {
+    
+  }
 };
 
 int main() {
-  std::string str{"ABAB"};
-  int k = 2;
+  // std::string str{"ABAB"};
+  // int k = 2;
+
+  std::string str{"AABABBA"};
+  int k = 1;
 
   solution soln;
   int len = soln.characterReplacement(str, k);
